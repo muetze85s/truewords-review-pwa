0bd6eed9511986249dc3f2ba489452e07327101d
muetze85s <56439144+muetze85s@users.noreply.github.com>
2026-08-06 04:23:59 +0700

    Refresh PWA cache for simplified interface

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='truewords-review-pwa-2026-v3';
+const CACHE='truewords-review-pwa-2026-v4';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg','./enhancements.css','./enhancements.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
